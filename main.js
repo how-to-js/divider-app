1c9c872b6925672d28735d984c2b4c40e1262418
Marcin Wosinek <marcin.wosinek@gmail.com>
2023-03-08 19:51:43 +0100

    add operation on pressing Enter

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-const numerator = document.querySelector("#numerator"),
+const body = document.querySelector("body"),
+  numerator = document.querySelector("#numerator"),
   denominator = document.querySelector("#denominator"),
   equals = document.querySelector("#equals"),
   result = document.querySelector("#result");
 
-equals.addEventListener("click", () => {
+function divide() {
   const numeratorValue = parseFloat(numerator.value),
     denominatorValue = parseFloat(denominator.value);
 
   const resultValue = numeratorValue / denominatorValue;
 
   result.innerHTML = resultValue;
+}
+
+body.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    divide();
+  }
 });
+
+equals.addEventListener("click", divide);
